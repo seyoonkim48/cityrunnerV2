5a595ed7eed12425bdd7e388f4a3f657af7dfedd
jmean12 <ooyhy123aa@gmail.com>
2021-11-23 23:36:25 +0900

    create SignIn UI

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Container, Title, InputContainer, SubmitContainer } from './SignUp';
+
+
 
 const SignIn = () => {
-    return <div>로그인 페이지</div>
-}
+    return (
+        <SignInContainer>
+        <SignInTitle>로그인</SignInTitle>
+        <SignInputContainer>
+        <h5>이메일 주소</h5>
+        <input name="email" type="email" placeholder="예) kimRunner@CityRunner.com"></input>
+        <h5>비밀번호</h5>
+        <input name="password" type="password" placeholder="비밀번호를 입력해주세요."></input>
+        </SignInputContainer>
+        <SignInSubmit>
+        <button>로그인</button>
+        </SignInSubmit>
+        </SignInContainer>
+    )
+};
+const SignInContainer = styled(Container)``;
+
+const SignInTitle = styled(Title)``;
+
+const SignInputContainer = styled(InputContainer)``;
+
+const SignInSubmit = styled(SubmitContainer)``;
+
+
 
 export default SignIn;
